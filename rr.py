0e15ec263c728bf05cb83323538c71b8dc02fb83
aaa <aaa@aaa.com>
2020-07-26 03:21:14 +0900

    change ble name

--- rr.py
+++ rr.py
@@ -16,7 +16,7 @@ GATT_CHRC_IFACE =			  'org.bluez.GattCharacteristic1'
 UART_SERVICE_UUID =			'6e400001-b5a3-f393-e0a9-e50e24dcca9e'
 UART_RX_CHARACTERISTIC_UUID =  '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
 UART_TX_CHARACTERISTIC_UUID =  '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
-LOCAL_NAME =				   'rpi-gatt-server'
+LOCAL_NAME =				   'NDT_DEV'
 mainloop = None
  
 
